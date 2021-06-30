8ba44995a803ed78f16d13f55e352622153751c6
ethanruity <eruity1@gmail.com>
2021-06-30 09:48:23 -0400

    Animation stuff

--- code.js
+++ code.js
@@ -1,6 +1,18 @@
 "use strict";
 // Your Code Here.
 
+//Just make a function to remove all the animation classes from the btns
+const clearClasses = function () {
+  btnOne.classList.remove("ball-one-black");
+};
+
+const hideBtns = function () {
+  for (const line of lines) {
+    line.classList.add("hidden");
+  }
+  btnContainer.classList.add("hidden");
+};
+
 //grab elemnets
 const btnContainer = document.querySelector("#btn-holder");
 const gameBoard = document.querySelector("#game-board");
@@ -89,6 +101,14 @@ playBtn.addEventListener("click", function () {
   renderBoard();
   userTurn();
   totalMoves = 0;
+  btnOneCount = 0;
+  btnTwoCount = 0;
+  btnThreeCount = 0;
+  btnFourCount = 0;
+  btnFiveCount = 0;
+  btnSixCount = 0;
+  btnSevenCount = 0;
+  clearClasses();
 });
 
 //Function to add to event listeners
@@ -104,34 +124,274 @@ const userMove = function (num) {
   return (totalMoves += 1);
 };
 
+//Variables for counting each click to adjust the animations
+let btnOneCount = 0;
+let btnTwoCount = 0;
+let btnThreeCount = 0;
+let btnFourCount = 0;
+let btnFiveCount = 0;
+let btnSixCount = 0;
+let btnSevenCount = 0;
+
 //Event listeners for placemennt buttons
 //Button one
 btnOne.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnOneCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnOneCount) {
+      case 0:
+        btnOne.classList.add("ball-one-black");
+        setTimeout(function () {
+          userMove(0);
+        }, 1000);
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(0);
+  bouncyBalls(btnOneCount, "one", 0);
+  btnOneCount += 1;
 });
+
 //Button two
 btnTwo.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnTwoCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnTwoCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(1);
+  btnTwoCount += 1;
 });
+
 //Button three
 btnThree.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnThreeCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnThreeCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(2);
+  btnThreeCount += 1;
 });
 //Button four
 btnFour.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnFourCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnFourCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(3);
+  btnFourCount += 1;
 });
 //Button five
 btnFive.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnFiveCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnFiveCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(4);
+  btnFiveCount += 1;
 });
 //Button six
 btnSix.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnSixCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnSixCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(5);
+  btnSixCount += 1;
 });
 //Button seven
 btnSeven.addEventListener("click", function () {
+  if (totalMoves % 2 == 0) {
+    switch (btnSevenCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  } else if (totalMoves % 2 === 1) {
+    switch (btnSevenCount) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+    }
+  }
   userMove(6);
+  btnSevenCount += 1;
 });
 
 // Needs work or maybe not
@@ -142,9 +402,6 @@ const userTurn = function () {
     }
   }
 };
-
-//Need to make functions to check for this
-const checkWinner = function () {};
 
 const updateBoard = function (column, user) {
   for (let i = board.length - 1; i >= 0; i--) {
@@ -157,10 +414,121 @@ const updateBoard = function (column, user) {
     }
   }
   renderBoard();
-  //need to turn into function
-  //refactor
-  // for (const line of lines) {
-  //   line.classList.remove("hidden");
-  // }
-  // btnContainer.classList.remove("hidden");
+};
+
+// isGameWon() function returns true if 4-in-a-row is found on the board
+let isGameWon = function () {
+  let won = false;
+  //set won as false so it will return false at end of function if it doesn't find a 4-in-a-row
+  const edgeX = board[0].length;
+  //x axis edge is fine as length
+  const edgeY = board.length - 3;
+  //made limit/edge for Y axis search, so diagonal and/or down searches don't go out of bounds
+  //length of each array is 6, but length of board array is only 5,
+  //also we're searching from top down, so we only need to search arrays 0 through 2 (so length of 5, -3, or board.length -3)
+  //to find any 4-in-a-row down and/or diagonal
+  // HORIZONTAL
+  // iterate each row
+
+  //For horizontal searches use entire board.length for y instead of edgeY
+  //so it can find any horizontal 4-in-a-rows on arrays/rows of indexes 3 through 5
+  for (let y = 0; y < board.length; y++) {
+    // iterate each cell in the row
+    for (let x = 0; x < edgeX; x++) {
+      let cell = board[y][x];
+
+      // Only check if cell is filled
+      if (cell !== 0) {
+        // Check the next three cells for the same value
+        if (
+          cell === board[y][x + 1] &&
+          cell === board[y][x + 2] &&
+          cell === board[y][x + 3]
+        ) {
+          console.log(
+            "4 in a row horizontal found at " + (x + 1) + ":" + (y + 1)
+          );
+
+          won = true;
+        }
+      }
+    }
+  }
+
+  // VERTICAL
+  // iterate each row
+  for (let y = 0; y < edgeY; y++) {
+    // iterate each cell in the row
+    for (let x = 0; x < edgeX; x++) {
+      let cell = board[y][x];
+
+      // Only check if cell is filled
+      if (cell !== 0) {
+        // Check the next three cells for the same value
+        if (
+          cell === board[y + 1][x] &&
+          cell === board[y + 2][x] &&
+          cell === board[y + 3][x]
+        ) {
+          console.log(
+            "4 in a row vertical found at " + (x + 1) + ":" + (y + 1)
+          );
+
+          won = true;
+        }
+      }
+    }
+  }
+
+  // DIAGONAL (DOWN RIGHT)
+  // iterate each row
+  for (let y = 0; y < edgeY; y++) {
+    // iterate each cell in the row
+    for (let x = 0; x < edgeX; x++) {
+      let cell = board[y][x];
+
+      // Only check if cell is filled
+      if (cell !== 0) {
+        // Check the next three cells for the same value
+        if (
+          cell === board[y + 1][x + 1] &&
+          cell === board[y + 2][x + 2] &&
+          cell === board[y + 3][x + 3]
+        ) {
+          console.log(
+            "4 in a row down-right found at " + (x + 1) + ":" + (y + 1)
+          );
+
+          won = true;
+        }
+      }
+    }
+  }
+
+  // DIAGONAL (DOWN LEFT)
+  // iterate each row
+  for (let y = 0; y < edgeY; y++) {
+    // iterate each cell in the row
+    for (let x = 0; x < edgeX; x++) {
+      let cell = board[y][x];
+
+      // Only check if cell is filled
+      if (cell !== 0) {
+        // Check the next three cells for the same value
+        if (
+          cell === board[y + 1][x - 1] &&
+          cell === board[y + 2][x - 2] &&
+          cell === board[y + 3][x - 3]
+        ) {
+          console.log(
+            "4 in a row down-left found at " + (x + 1) + ":" + (y + 1)
+          );
+
+          won = true;
+        }
+      }
+    }
+  }
+  // console.log("Game Won? " + won);
+  return won;
 };
